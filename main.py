5050d8a99c978feb879f7602fac88ef5f564816d
Reda Benjelloun <redabenjelloun24@gmail.com>
2025-10-02 18:39:55 +0100

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from MLProject import logger  # Thanks to __init__.py, we can directly call MLProject instead of src.MLProject
+from MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our Home Page")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e

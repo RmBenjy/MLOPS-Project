b3b221422c9109efb745c119cdda6bc0d5c8f1fd
Reda Benjelloun <redabenjelloun24@gmail.com>
2025-10-05 18:14:33 +0100

    data validation added

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from MLProject import logger  # Thanks to __init__.py, we can directly call MLProject instead of src.MLProject
 from MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from MLProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n\nx============x")
 except Exception as e:

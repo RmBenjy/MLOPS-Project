17949c6a90f81de3756b31dd6a8626c552eab2a2
Reda Benjelloun <redabenjelloun24@gmail.com>
2025-10-08 21:44:28 +0100

    model evaluation added

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
+
+# Set MLflow environment variables
+os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/RmBenjy/MLOPS-Project.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "RmBenjy"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "ba8c84e91b9a47bfeb4b083b748f8e057a71c22a"
+
 from MLProject import logger  # Thanks to __init__.py, we can directly call MLProject instead of src.MLProject
 from MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from MLProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from MLProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -14,8 +22,6 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
-
-
 
 STAGE_NAME = "Data Validation Stage"
 
@@ -44,6 +50,17 @@ STAGE_NAME = "Model Trainer Stage"
 try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n\nx============x")
 except Exception as e:
---
+++ src/MLProject/components/model_evaluation.py
@@ -0,0 +1,53 @@
+import os
+import joblib
+from pathlib import Path
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import pandas as pd
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+from MLProject.entity.config_entity import ModelEvaluationConfig
+from MLProject.utils.common import save_json
+
+class ModelEvaluation:
+    def __init__(self, config: ModelEvaluationConfig):
+        self.config = config
+
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
+    def log_into_mlflow(self):
+        test_data = pd.read_csv(self.config.test_data_path)
+        model = joblib.load(self.config.model_path)
+
+        X_test = test_data.drop([self.config.target_column], axis=1)
+        y_test = test_data[[self.config.target_column]]
+
+        mlflow.set_registry_uri(self.config.mlflow_uri)
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        with mlflow.start_run():
+            predicted_qualities = model.predict(X_test)
+
+            (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+            # Saving metrics as local
+            scores = {"rmse":rmse, "mae":mae, "r2":r2}
+            save_json(path=Path(self.config.metric_file_name), data=scores)
+
+            mlflow.log_params(self.config.all_params)
+
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_metric("mae", mae)
+            mlflow.log_metric("r2", r2)
+
+            # Model registry doesn't work with file store
+            if tracking_url_type_store != "file":
+                mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+            else:
+                mlflow.sklearn.log_model(model, "model")

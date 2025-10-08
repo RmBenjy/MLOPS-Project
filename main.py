07875fe656a1a1a546ba4d0b0f99db29dc913f3d
Reda Benjelloun <redabenjelloun24@gmail.com>
2025-10-08 18:10:49 +0100

    model_trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from MLProject import logger  # Thanks to __init__.py, we can directly call MLPr
 from MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -32,6 +33,17 @@ STAGE_NAME = "Data Transformation Stage"
 try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj = ModelTrainerTrainingPipeline()
     obj.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n\nx============x")
 except Exception as e:
